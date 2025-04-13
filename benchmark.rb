97a04426b0f30ab9f3cab9c2945eb56d4350d687
James Prior <jamesgr.prior@gmail.com>
2025-04-13 14:34:26 +0100

    Accept a benchmark fixute name command line argument

--- benchmark.rb
+++ benchmark.rb
@@ -2,8 +2,9 @@
 
 require "benchmark/ips"
 require "json"
-require "pathname"
 require "liquid"
+require "optparse"
+require "pathname"
 
 MemoryFileSystem = Struct.new(:templates) do
   def read_template_file(path)
@@ -13,10 +14,11 @@ end
 
 # A benchmark fixture
 class Fixture
-  attr_reader :templates, :data
+  attr_reader :templates, :data, :name
 
+  # @param path [Pathname]
   def initialize(path)
-    @root = Pathname.new(path)
+    @root = path
     @name = @root.basename.to_s
     @data = JSON.parse((@root + "data.json").read)
     @templates = (@root + "templates").glob("*liquid").to_h do |p|
@@ -31,7 +33,25 @@ class Fixture
   end
 end
 
-fixture = Fixture.new("benchmark_fixtures/002")
+options = {
+  fixture: "002"
+}
+
+OptionParser.new do |parser|
+  parser.banner = <<~BANNER
+    Run one of the benchmarks in ./benchmark_fixtures.
+    Example: ruby benchmark.rb -f 002
+  BANNER
+
+  parser.on("-f FIXTURE", "--fixture FIXTURE",
+            "The name of the benchmark fixture to run. Defaults to '002'.") do |value|
+    options[:fixture] = value
+  end
+
+  parser.parse!
+end
+
+fixture = Fixture.new(Pathname.new("benchmark_fixtures") + options[:fixture])
 env = fixture.env
 source = fixture.templates["index.liquid"]
 template = Liquid::Template.parse(source, environment: env, line_numbers: true)
@@ -41,15 +61,15 @@ Benchmark.ips do |x|
   # the warmup phase (default 2) and calculation phase (default 5)
   x.config(warmup: 2, time: 5)
 
-  x.report("parse:") do
+  x.report("parse (#{fixture.name}):") do
     Liquid::Template.parse(source, environment: env, line_numbers: true)
   end
 
-  x.report("render:") do
+  x.report("render (#{fixture.name}):") do
     template.render!
   end
 
-  x.report("parse and render:") do
+  x.report("both (#{fixture.name}):") do
     Liquid::Template.parse(source, environment: env, line_numbers: true).render
   end
 end
